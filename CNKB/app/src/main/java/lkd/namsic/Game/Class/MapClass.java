676f6604b929cc9657e69736fef9f51334c227ba
user <namsic6460@gmail.com>
2021-03-31 14:10:04 +0900

    Move entity variables to map variable

--- CNKB/app/src/main/java/lkd/namsic/Game/Class/MapClass.java
+++ CNKB/app/src/main/java/lkd/namsic/Game/Class/MapClass.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,6 +15,7 @@ import lkd.namsic.Game.Config;
 import lkd.namsic.Game.Enum.Id;
 import lkd.namsic.Game.Enum.MapType;
 import lkd.namsic.Game.Exception.NumberRangeException;
+import lkd.namsic.Game.Exception.ObjectNotFoundException;
 import lkd.namsic.Game.Exception.WeirdDataException;
 import lombok.Getter;
 import lombok.Setter;
@@ -36,15 +38,14 @@ public class MapClass implements Serializable {
 
     Location location;
 
+    ConcurrentHashMap<Location, Long> money;
     ConcurrentHashMap<Location, ConcurrentHashMap<Long, Integer>> item;
     ConcurrentHashMap<Location, ConcurrentHashSet<Long>> equip;
 
-    ConcurrentHashSet<Long> boss;
-    ConcurrentHashSet<Long> monster;
-    ConcurrentHashSet<Long> npc;
-    ConcurrentHashSet<Long> player;
+    ConcurrentHashMap<Id, ConcurrentHashSet<Long>> entity;
 
     public MapClass(@NonNull String name, boolean pvp, @NonNull MapType mapType, @NonNull Location location,
+                    @NonNull ConcurrentHashMap<Location, Long> money,
                     @NonNull ConcurrentHashMap<Location, ConcurrentHashMap<Long, Integer>> item,
                     @NonNull ConcurrentHashMap<Location, ConcurrentHashSet<Long>> equip,
                     @NonNull ConcurrentHashSet<Long> boss, @NonNull ConcurrentHashSet<Long> monster,
@@ -54,12 +55,20 @@ public class MapClass implements Serializable {
         this.mapType = mapType;
         this.location = location;
 
+        this.setMoney(money);
         this.setItem(item);
         this.addEquip(equip);
-        this.addBoss(boss);
-        this.addMonster(monster);
-        this.addNpc(npc);
-        this.addPlayer(player);
+
+        this.entity = new ConcurrentHashMap<>();
+        this.entity.put(Id.BOSS, new ConcurrentHashSet<>());
+        this.entity.put(Id.MONSTER, new ConcurrentHashSet<>());
+        this.entity.put(Id.NPC, new ConcurrentHashSet<>());
+        this.entity.put(Id.PLAYER, new ConcurrentHashSet<>());
+
+        this.addEntity(Id.BOSS, boss);
+        this.addEntity(Id.MONSTER, monster);
+        this.addEntity(Id.NPC, npc);
+        this.addEntity(Id.PLAYER, player);
     }
 
     @NonNull
@@ -85,22 +94,22 @@ public class MapClass implements Serializable {
                 }
             }
         }
-        for(Long bossId : boss) {
+        for(Long bossId : this.getEntity(Id.BOSS)) {
             if(location.equalsField(((Boss)Config.getData(Id.BOSS, bossId)).getLocation())) {
                 bossMap.put(bossId, 1);
             }
         }
-        for(Long monsterId : monster) {
+        for(Long monsterId : this.getEntity(Id.MONSTER)) {
             if(location.equalsField(((Monster)Config.getData(Id.MONSTER, monsterId)).getLocation())) {
                 monsterMap.put(monsterId, 1);
             }
         }
-        for(Long npcId : npc) {
+        for(Long npcId : this.getEntity(Id.NPC)) {
             if(location.equalsField(((Npc)Config.getData(Id.NPC, npcId)).getLocation())) {
                 npcMap.put(npcId, 1);
             }
         }
-        for(Long playerId : player) {
+        for(Long playerId : this.getEntity(Id.PLAYER)) {
             if(location.equalsField(((Player)Config.getData(Id.PLAYER, playerId)).getLocation())) {
                 playerMap.put(playerId, 1);
             }
@@ -141,16 +150,16 @@ public class MapClass implements Serializable {
                 equipMap.put(equipId, 1);
             }
         }
-        for(Long bossId : boss) {
+        for(Long bossId : this.getEntity(Id.BOSS)) {
             bossMap.put(bossId, 1);
         }
-        for(Long monsterId : monster) {
+        for(Long monsterId : this.getEntity(Id.MONSTER)) {
             monsterMap.put(monsterId, 1);
         }
-        for(Long npcId : npc) {
+        for(Long npcId : this.getEntity(Id.NPC)) {
             npcMap.put(npcId, 1);
         }
-        for(Long playerId : player) {
+        for(Long playerId : this.getEntity(Id.PLAYER)) {
             playerMap.put(playerId, 1);
         }
 
@@ -161,6 +170,38 @@ public class MapClass implements Serializable {
         map.put(Id.NPC, npcMap);
         map.put(Id.PLAYER, playerMap);
         return map;
+    }
+
+    public void setMoney(Map<Location, Long> money) {
+        for(Map.Entry<Location, Long> entry : money.entrySet()) {
+            this.setMoney(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void setMoney(Location location, long money) {
+        if(!location.equalsMap(this.location)) {
+            throw new WeirdDataException(this.location, location);
+        }
+
+        if(money < 0) {
+            throw new NumberRangeException(money, 0);
+        }
+
+        this.money.put(location, this.getMoney(location) + money);
+    }
+
+    public long getMoney(Location location) {
+        Long value = this.money.get(location);
+
+        if(value != null) {
+            return value;
+        } else {
+            return 0;
+        }
+    }
+
+    public void addMoney(Location location, long money) {
+        this.setMoney(location, this.getMoney(location) + money);
     }
 
     public void setItem(Map<Location, ConcurrentHashMap<Long, Integer>> item) {
@@ -249,64 +290,34 @@ public class MapClass implements Serializable {
         }
     }
 
-    public void addBoss(Set<Long> boss) {
-        for(long bossId : boss) {
-            this.addBoss(bossId);
+    @NonNull
+    public Set<Long> getEntity(Id id) {
+        return Objects.requireNonNull(this.entity.get(id));
+    }
+
+    public void addEntity(Id id, Set<Long> entity) {
+        for(long objectId : entity) {
+            this.addEntity(id, objectId);
         }
     }
 
-    public void addBoss(long bossId) {
-        Boss boss = Config.getData(Id.BOSS, bossId);
-        if(!boss.getLocation().equalsMap(this.location)) {
-            throw new WeirdDataException(this.location, boss.getLocation());
-        }
+    public void addEntity(Id id, long objectId) {
+        Id.checkEntityId(id);
+        Config.checkId(id, objectId);
 
-        this.boss.add(bossId);
+        this.getEntity(id).add(objectId);
     }
 
-    public void addMonster(Set<Long> monster) {
-        for(long monsterId : monster) {
-            this.addMonster(monsterId);
+    public void removeEntity(Id id, long objectId) {
+        Id.checkEntityId(id);
+        Config.checkId(id, objectId);
+
+        Set<Long> entitySet = this.getEntity(id);
+        if(entitySet.contains(objectId)) {
+            entitySet.remove(objectId);
+        } else {
+            throw new ObjectNotFoundException(id, objectId);
         }
-    }
-
-    public void addMonster(long monsterId) {
-        Monster monster = Config.getData(Id.MONSTER, monsterId);
-        if(!monster.getLocation().equalsMap(this.location)) {
-            throw new WeirdDataException(this.location, monster.getLocation());
-        }
-
-        this.monster.add(monsterId);
-    }
-
-    public void addNpc(Set<Long> npc) {
-        for(long npcId : npc) {
-            this.addNpc(npcId);
-        }
-    }
-
-    public void addNpc(long npcId) {
-        Npc npc = Config.getData(Id.NPC, npcId);
-        if(!npc.getLocation().equalsMap(this.location)) {
-            throw new WeirdDataException(this.location, npc.getLocation());
-        }
-
-        this.npc.add(npcId);
-    }
-
-    public void addPlayer(Set<Long> player) {
-        for(long playerId : player) {
-            this.addPlayer(playerId);
-        }
-    }
-
-    public void addPlayer(long playerId) {
-        Player player = Config.getData(Id.PLAYER, playerId);
-        if(!player.getLocation().equalsMap(this.location)) {
-            throw new WeirdDataException(this.location, player.getLocation());
-        }
-
-        this.player.add(playerId);
     }
 
 }
