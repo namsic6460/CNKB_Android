02e1d7525375251f0c5bb9dd3828d354d29c9de8
user <namsic6460@gmail.com>
2021-04-06 12:24:59 +0900

    Delete comples + unnecessary methods

--- CNKB/app/src/main/java/lkd/namsic/Game/GameObject/MapClass.java
+++ CNKB/app/src/main/java/lkd/namsic/Game/GameObject/MapClass.java
@@ -2,13 +2,13 @@ package lkd.namsic.Game.GameObject;
 
 import androidx.annotation.NonNull;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import lkd.namsic.Game.Base.ConcurrentHashSet;
+import lkd.namsic.Game.Base.LimitInteger;
 import lkd.namsic.Game.Base.Location;
 import lkd.namsic.Game.Config;
 import lkd.namsic.Game.Enum.Id;
@@ -34,6 +34,8 @@ public class MapClass {
     @NonNull
     MapType mapType = MapType.COUNTRY;
 
+    LimitInteger requireLv = new LimitInteger(Config.MIN_LV, Config.MIN_LV, Config.MAX_LV);
+
     Location location = new Location();
 
     Map<Location, Long> money = new ConcurrentHashMap<>();
@@ -44,113 +46,6 @@ public class MapClass {
 
     public MapClass(@NonNull String name) {
         this.name = name;
-    }
-
-    @NonNull
-    public Map<Id, HashMap<Long, Integer>> getFieldData(Location location) {
-        Map<Id, HashMap<Long, Integer>> map = new HashMap<>();
-
-        HashMap<Long, Integer> itemMap = new HashMap<>();
-        HashMap<Long, Integer> equipMap = new HashMap<>();
-        HashMap<Long, Integer> bossMap = new HashMap<>();
-        HashMap<Long, Integer> monsterMap = new HashMap<>();
-        HashMap<Long, Integer> npcMap = new HashMap<>();
-        HashMap<Long, Integer> playerMap = new HashMap<>();
-
-        for(Map.Entry<Location, ConcurrentHashMap<Long, Integer>> entry : item.entrySet()) {
-            if(location.equalsField(entry.getKey())) {
-                itemMap.putAll(entry.getValue());
-            }
-        }
-        for(Map.Entry<Location, ConcurrentHashSet<Long>> entry : equip.entrySet()) {
-            if(location.equalsField(entry.getKey())) {
-                for(Long equipId : entry.getValue()) {
-                    equipMap.put(equipId, 1);
-                }
-            }
-        }
-        for(Long bossId : this.getEntity(Id.BOSS)) {
-            if(location.equalsField(((Boss)Config.getData(Id.BOSS, bossId)).getLocation())) {
-                bossMap.put(bossId, 1);
-            }
-        }
-        for(Long monsterId : this.getEntity(Id.MONSTER)) {
-            if(location.equalsField(((Monster)Config.getData(Id.MONSTER, monsterId)).getLocation())) {
-                monsterMap.put(monsterId, 1);
-            }
-        }
-        for(Long npcId : this.getEntity(Id.NPC)) {
-            if(location.equalsField(((Npc)Config.getData(Id.NPC, npcId)).getLocation())) {
-                npcMap.put(npcId, 1);
-            }
-        }
-        for(Long playerId : this.getEntity(Id.PLAYER)) {
-            if(location.equalsField(((Player)Config.getData(Id.PLAYER, playerId)).getLocation())) {
-                playerMap.put(playerId, 1);
-            }
-        }
-
-        map.put(Id.ITEM, itemMap);
-        map.put(Id.EQUIPMENT, equipMap);
-        map.put(Id.BOSS, bossMap);
-        map.put(Id.MONSTER, monsterMap);
-        map.put(Id.NPC, npcMap);
-        map.put(Id.PLAYER, playerMap);
-        return map;
-    }
-
-    @NonNull
-    public Map<Id, HashMap<Long, Integer>> getMapData(Location location) {
-        Map<Id, HashMap<Long, Integer>> map = new HashMap<>();
-
-        HashMap<Long, Integer> itemMap = new HashMap<>();
-        HashMap<Long, Integer> equipMap = new HashMap<>();
-        HashMap<Long, Integer> bossMap = new HashMap<>();
-        HashMap<Long, Integer> monsterMap = new HashMap<>();
-        HashMap<Long, Integer> npcMap = new HashMap<>();
-        HashMap<Long, Integer> playerMap = new HashMap<>();
-
-        for(ConcurrentHashMap<Long, Integer> item : this.item.values()) {
-            for(Map.Entry<Long, Integer> entry : item.entrySet()) {
-                long objectId = entry.getKey();
-
-                Integer value = itemMap.get(objectId);
-                value = value == null ? 0 : value;
-
-                itemMap.put(objectId, value + entry.getValue());
-            }
-        }
-        for(ConcurrentHashSet<Long> equip : this.equip.values()) {
-            for(long equipId : equip) {
-                equipMap.put(equipId, 1);
-            }
-        }
-        for(Long bossId : this.getEntity(Id.BOSS)) {
-            bossMap.put(bossId, 1);
-        }
-        for(Long monsterId : this.getEntity(Id.MONSTER)) {
-            monsterMap.put(monsterId, 1);
-        }
-        for(Long npcId : this.getEntity(Id.NPC)) {
-            npcMap.put(npcId, 1);
-        }
-        for(Long playerId : this.getEntity(Id.PLAYER)) {
-            playerMap.put(playerId, 1);
-        }
-
-        map.put(Id.ITEM, itemMap);
-        map.put(Id.EQUIPMENT, equipMap);
-        map.put(Id.BOSS, bossMap);
-        map.put(Id.MONSTER, monsterMap);
-        map.put(Id.NPC, npcMap);
-        map.put(Id.PLAYER, playerMap);
-        return map;
-    }
-
-    public void setMoney(Map<Location, Long> money) {
-        for(Map.Entry<Location, Long> entry : money.entrySet()) {
-            this.setMoney(entry.getKey(), entry.getValue());
-        }
     }
 
     public void setMoney(Location location, long money) {
@@ -177,18 +72,6 @@ public class MapClass {
 
     public void addMoney(Location location, long money) {
         this.setMoney(location, this.getMoney(location) + money);
-    }
-
-    public void setItem(Map<Location, ConcurrentHashMap<Long, Integer>> item) {
-        for(Map.Entry<Location, ConcurrentHashMap<Long, Integer>> entry : item.entrySet()) {
-            this.setItem(entry.getKey(), entry.getValue());
-        }
-    }
-
-    public void setItem(Location location, ConcurrentHashMap<Long, Integer> item) {
-        for(Map.Entry<Long, Integer> entry : item.entrySet()) {
-            this.setItem(location, entry.getKey(), entry.getValue());
-        }
     }
 
     public void setItem(Location location, long itemId, int count) {
@@ -236,18 +119,6 @@ public class MapClass {
         this.setItem(location, itemId, this.getItem(location, itemId) + count);
     }
 
-    public void addEquip(Map<Location, ConcurrentHashSet<Long>> equip) {
-        for(Map.Entry<Location, ConcurrentHashSet<Long>> entry : equip.entrySet()) {
-            this.addEquip(entry.getKey(), entry.getValue());
-        }
-    }
-
-    public void addEquip(Location location, Set<Long> equip) {
-        for(long equipId : equip) {
-            this.addEquip(location, equipId);
-        }
-    }
-
     public void addEquip(Location location, long equipId) {
         if(!location.equalsMap(this.location)) {
             throw new WeirdDataException(this.location, location);
@@ -268,12 +139,6 @@ public class MapClass {
     @NonNull
     public Set<Long> getEntity(Id id) {
         return Objects.requireNonNull(this.entity.get(id));
-    }
-
-    public void addEntity(Id id, Set<Long> entity) {
-        for(long objectId : entity) {
-            this.addEntity(id, objectId);
-        }
     }
 
     public void addEntity(Id id, long objectId) {
