8a9993b5af300545f52068d2400c79d29b29a8a8
user <namsic6460@gmail.com>
2021-04-28 16:33:50 +0900

    Add registering

--- CNKB/app/src/main/java/lkd/namsic/service/KakaoTalk.java
+++ CNKB/app/src/main/java/lkd/namsic/service/KakaoTalk.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.app.RemoteInput;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,7 +26,13 @@ public class KakaoTalk {
     private final static Map<String, Notification.Action> groupSessions = new ConcurrentHashMap<>();
     private final static Map<String, Notification.Action> soloSessions = new ConcurrentHashMap<>();
 
+    private static String lastSender = "";
+    private static String lastMsg = "";
+
     public static Notification.Action getGroupSession(String room) {
+        Log.i("namsic!", room);
+        Log.i("namsic!", groupSessions.toString());
+        Log.i("namsic!", soloSessions.toString());
         return Objects.requireNonNull(groupSessions.get(room));
     }
 
@@ -49,20 +56,25 @@ public class KakaoTalk {
             Player player = null;
 
             try {
-                Logger.i("chatLog", "sender: " + sender + "\nimage: " + image +
-                        "\nmsg: " + msg + "\nroom: " + room + "\nisGroupChat: " + isGroupChat +
-                        "\ncommand: " + command);
-                if (msg.equals("namsicTest")) {
-                    reply(session, "reply");
+                if(lastSender.equals(sender) && lastMsg.equals(msg)) {
+                    Logger.i("chatLog", "ReChat");
+                } else {
+                    Logger.i("chatLog", "\nsender: " + sender + "\nmsg: " +
+                            msg.substring(0, Math.min(msg.length(), 100)).trim() +
+                            "\nroom: " + room + "\nisGroupChat: " + isGroupChat);
                 }
+
+                lastSender = sender;
+                lastMsg = msg;
 
                 if(isCommand) {
                     boolean isEnd;
                     player = Config.loadPlayer(sender, image);
-                    if (player == null) {
-                        isEnd = playerCommand(sender, image, command, room, session);
+
+                    if (player != null) {
+                        isEnd = playerCommand(player, command, room, isGroupChat, session);
                     } else {
-                        isEnd = nonPlayerCommand(sender, image, command, room, session);
+                        isEnd = nonPlayerCommand(sender, image, command, room, isGroupChat, session);
                     }
 
                     if (!isEnd) {
@@ -131,17 +143,32 @@ public class KakaoTalk {
         }
     }
 
-    private static boolean playerCommand(@NonNull String sender, @NonNull String image,
-                                         @NonNull String command, @NonNull String room,
-                                         @NonNull Notification.Action session) {
+    private static boolean playerCommand(@NonNull Player player, @NonNull String command, @NonNull String room,
+                                         boolean isGroupChat, @NonNull Notification.Action session) {
+        if(!player.checkChat()) {
+            return true;
+        }
 
+        player.setRecentRoom(room);
+        player.setGroup(isGroupChat);
 
+        try {
+            String[] commands = command.split(" ");
+
+            if (Arrays.asList("회원가입", "가입", "register").contains(commands[0])) {
+                reply(session, "이미 회원가입이 되어 있습니다.\n" +
+                        "회원가입을 진행한 적이 없는 경우, 프로필 이미지 또는 카카오톡 이름을 변경한 후 다시 시도해주세요");
+            }
+        } catch (WeirdCommandException e) {
+            reply(session, Objects.requireNonNull(e.getMessage()));
+        }
+        
         return false;
     }
 
     private static boolean nonPlayerCommand(@NonNull String sender, @NonNull String image,
-                                         @NonNull String command, @NonNull String room,
-                                         @NonNull Notification.Action session) {
+                                            @NonNull String command, @NonNull String room,
+                                            boolean isGroupChat, @NonNull Notification.Action session) {
         try {
             String[] commands = command.split(" ");
 
@@ -151,7 +178,17 @@ public class KakaoTalk {
                 }
 
                 String nickName = commands[1];
-                //닉네임 검사
+                if(Config.NICKNAME_LIST.contains(nickName)) {
+                    reply(session, "[" + nickName + "]\n동일한 닉네임을 지닌 플레이어가 존재합니다.\n" +
+                            "닉네임을 변경한 후 다시 시도해주세요");
+                }
+
+                Player player = new Player(sender, nickName, image, room);
+                player.setMoney(10);
+                player.setGroup(isGroupChat);
+                Config.unloadObject(player);
+
+                player.replyPlayer("회원가입에 성공하였습니다!");
 
                 return true;
             }
